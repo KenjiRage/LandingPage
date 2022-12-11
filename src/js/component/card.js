3f1f3b55d19768f04c521d0b91a058a0479ce143
KenjiRage <kenjijevidelmetal@gmail.com>
2022-12-11 23:23:15 +0000

    Add cards

---
+++ src/js/component/card.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Card = () => {
+	return (
+        <div className="col">
+		    <div className="card" style="witdh: 20rem;">
+                <img scr="..." class="card-img-top" alt="..."/>
+			        <div className="card-body">
+                        <h4 className="card-title">Card title</h4>
+                        <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada urna vitae turpis convallis molestie vel eu risus.</p>
+                        <a href="#" class="btn btn-primary">Find Out More!</a>
+                    </div>
+            </div>
+		</div>
+	
+	);
+};
+
+export default Card;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,6 +2,9 @@ import React from "react";
 
 import Navbar from "./navbar.js";
 import Jumbotron from "./jumbotron.js";
+import Card from "./card.js";
+
+
 
 //create your first component
 const Home = () => {
@@ -10,6 +13,12 @@ const Home = () => {
 		<div className="text-center">
 			<Navbar/>
 			<Jumbotron/>
+			<div className="container"></div>
+			<div className="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4"></div>
+			<Card/>
+			<Card/>
+			<Card/>
+			<Card/>
 		</div>
 	
 	);

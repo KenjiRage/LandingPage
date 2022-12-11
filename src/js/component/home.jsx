0bcfed8df4746159cbfc25c4aef1521f8438fecf
KenjiRage <kenjijevidelmetal@gmail.com>
2022-12-11 23:50:00 +0000

    Add footer

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import Navbar from "./navbar.js";
 import Jumbotron from "./jumbotron.js";
 import Card from "./card.js";
+import Footer from "./footer.js";
 
 
 
@@ -15,10 +16,13 @@ const Home = () => {
 			<Jumbotron/>
 			<div className="container"></div>
 			<div className="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4"></div>
-			<Card/>
-			<Card/>
-			<Card/>
-			<Card/>
+				<Card/>
+				<Card/>
+				<Card/>
+				<Card/>
+			<div className="text-center h-100">
+				<Footer/>
+			</div>
 		</div>
 	
 	);

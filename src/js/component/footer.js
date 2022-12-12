78709676925b52561e4bea0533ad1af4d1f44831
KenjiRage <kenjijevidelmetal@gmail.com>
2022-12-12 12:46:20 +0000

    cambios en card

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -1,15 +1,11 @@
 import React from "react";
 
-
-
-
-//create your first component
 const Footer = () => {
 	return (
 		
 		<footer className="footer sticky-bottom py-3 bg-dark">
 			<div className="container">
-                <span className="text-muted">Copyright @ You rWebsite 2019</span>
+                <span className="text-muted">Copyright @ Your Website 2019</span>
             </div>
     </footer>
 	

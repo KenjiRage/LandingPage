9d88d55dea055cd05224b402291582d09e3bd237
KenjiRage <68568868+KenjiRage@users.noreply.github.com>
2022-12-12 13:49:54 +0100

    Update home.jsx

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,10 +2,8 @@ import React from "react";
 
 import Navbar from "./navbar.js";
 import Jumbotron from "./jumbotron.js";
-import Card from "./card.js";
+import Card from "./card.jsx";
 import Footer from "./footer.js";
-
-
 
 //create your first component
 const Home = () => {
@@ -14,18 +12,43 @@ const Home = () => {
 		<div className="text-center">
 			<Navbar/>
 			<Jumbotron/>
-			<div className="container"></div>
-			<div className="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4"></div>
-				<Card/>
-				<Card/>
-				<Card/>
-				<Card/>
-			<div className="text-center h-100">
-				<Footer/>
+			<div className="container">
+			<div className="row row cols-1 row-cols-md-2 row-cols-lg-4 g-4">
+					<Card
+						img="https://edgewoodreit.com/wp-content/uploads/2018/01/500x325.png"
+						title="Card Title"
+						content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sapien libero, tincidunt quis dolor a, rhoncus maximus urna. Aenean dictum orci in faucibus varius."
+						button="Find Out More!"
+						href=""
+					/>
+					<Card
+						img="https://edgewoodreit.com/wp-content/uploads/2018/01/500x325.png"
+						title="Card Title"
+						content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sapien libero, tincidunt quis dolor a, rhoncus maximus urna. Aenean dictum orci in faucibus varius."
+						button="Find Out More!"
+						href=""
+					/>
+					<Card
+						img="https://edgewoodreit.com/wp-content/uploads/2018/01/500x325.png"
+						title="Card Title"
+						content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sapien libero, tincidunt quis dolor a, rhoncus maximus urna. Aenean dictum orci in faucibus varius."
+						button="Find Out More!"
+						href=""
+					/>
+					<Card
+						img="https://edgewoodreit.com/wp-content/uploads/2018/01/500x325.png"
+						title="Card Title"
+						content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sapien libero, tincidunt quis dolor a, rhoncus maximus urna. Aenean dictum orci in faucibus varius."
+						button="Find Out More!"
+						href=""
+					/>
+				</div>
 			</div>
+			<Footer/>
 		</div>
 	
 	);
 };
 
 export default Home;
+
